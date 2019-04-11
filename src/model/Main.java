70492dcf77d70c780d42d2379bd0163f669bbaac
ali.aliabadi <al.aliabadi@mail.sbu.ac.ir>
2019-04-11 23:13:31 +0430

    change Main.java method for loading pages

--- src/model/Main.java
+++ src/model/Main.java
@@ -1,19 +1,15 @@
 package model;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
+
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/view/sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+        PageLoader.intiStage(primaryStage);
+        new PageLoader().load("/view/Login.fxml");
     }
 
 
