52417463814188233542f8da1247012e37b03e21
ali.aliabadi <al.aliabadi@mail.sbu.ac.ir>
2019-04-12 01:45:10 +0430

    fixing user class file and creting Tweet class

--- src/model/Tweet/Tweet.java
+++ src/model/Tweet/Tweet.java
@@ -1,8 +1,65 @@
 package model.Tweet;
 
 
+import java.util.ArrayList;
+
 public class Tweet {
-    public void print() {
-        ;
+
+    private int id;         // can only be number and its unic
+    private String text;    // maximum 140 charectars
+    private ArrayList<String> userLiked = new ArrayList<String>();
+
+
+    /**
+     * constractors
+     */
+
+    Tweet(int id, String text) {
+        this.id = id;       // it is seted because the tweet ids start from 0 and new id is previousId + 1 and it is readen from db
+        this.text = text;
+    }
+
+
+    /**
+     * setters and adder
+     */
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void addUserLiked(String userId) {
+        this.userLiked.add(userId);
+    }
+
+
+    /**
+     * getters and delleters
+     */
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getText() {
+        return this.text;
+    }
+
+    public void deleteLikedUser(String userId) {
+        int len = this.numberOfLikes();
+        for (int i = 0; i < len; i++) {
+            if (userId.equals(this.userLiked.get(i))) {
+                this.userLiked.remove(i);
+                break;
+            }
+        }
+    }
+
+    public int numberOfLikes() {
+        return this.userLiked.size();
     }
 }
--- src/model/User/User.java
+++ src/model/User/User.java
@@ -1,8 +1,6 @@
 package model.User;
 
 import java.util.ArrayList;
-import java.util.Scanner;
-import model.Tweet.Tweet;
 
 public class User {
     
@@ -12,13 +10,11 @@ public class User {
     private String email;
     private String bio;
 
-    private ArrayList <User> followers = new ArrayList <User>();
+    private ArrayList <String> followers = new ArrayList <String>();
 
-    private ArrayList <User> following = new ArrayList <User>();
+    private ArrayList <String> following = new ArrayList <String>();
 
-    private ArrayList <Tweet> tweets = new ArrayList <Tweet>();
-
-    private ArrayList <User> blockUsers = new ArrayList <User>();
+    private ArrayList <Integer> tweets = new ArrayList <Integer>();
 
 
     /** constractors */
@@ -43,39 +39,32 @@ public class User {
     }
 
     public void setEmail(String email) {
-
         /**
-         * email validation need here
+         * checkhing of valid email needed
          */
-
         this.email = email;
     }
 
     public void setBio(String bio) {
 
         /**
-         * checking bio be less than 161 words needed here
+         * checking bio be less than 161 words needed
          */
 
         this.bio = bio;
     }
 
-    public void addFollowing(User user) {
-        following.add(user);
+    public void addFollowing(String userid) {
+        following.add(userid);
     }
 
-    public void addFollower(User user) {
-        following.add(user);
+    public void addFollower(String userid) {
+        following.add(userid);
     }
 
-    public void addTweet(Tweet tweet) {
-        tweets.add(tweet);
+    public void addTweet(int tweetid) {
+        tweets.add(tweetid);
     }
-
-    public void addBlockUser(User user) {
-        blockUsers.add(user);
-    }
-
 
     /** getters and deleters(delete follower and delete following) */
 
@@ -111,144 +100,23 @@ public class User {
         return tweets.size();
     }
 
-    public int getBlockUsersNumber() {
-        return blockUsers.size();
-    }
+    public void deleteFollowing(String userid) {
 
-    public void deleteFollowing(User user) {
         int len = followers.size();
         for (int i = 0; i < len; i++) {
-            if (user == followers.get(i)) {
+            if (userid.equals(followers.get(i))) {
                 followers.remove(i);
                 break;
             }
         }
     }
 
-    public void deleteBlockUser(User user) {
-        int len = blockUsers.size();
-        for (int i = 0; i < len; i++) {
-            if (user == blockUsers.get(i)) {
-                blockUsers.remove(i);
-                break;
-            }
-        }
-    }
-
-
     /** other methodes */
 
     @Override
     public boolean equals(Object obj) {
-        
         User user = (User) obj;
-        return this.getId() == user.getId();
+        return this.getId().equals(user.getId());
     }
 
-    public void myProfile() {
-        /**
-         * this method must change
-         */
-        int flag = 0;
-        System.out.println("My id : " + id);
-        
-        if (name != null) {
-            System.out.println("Name : " + name);
-            flag ++;
-        }
-        if (email != null) {
-            System.out.println("Email : " + email);
-            flag ++;
-        }
-        if (bio != null) {
-            System.out.println("Bio : " + bio);
-            flag ++;
-        }
-
-        System.out.println("Number of followers : " + followers.size());
-        System.out.println("Number of followings : " + following.size());
-
-        int len = tweets.size();
-        for (int i = 0; i < len; i++) {
-            tweets.get(i).print();
-        }
-
-        if (flag < 3) {
-            System.out.println("Your profile is not complete, do you want to complete it now?(Y for yes and N for no)");
-            Scanner in = new Scanner(System.in);
-            if (in.next().toLowerCase().equals("y")) {
-                this.completeProfile();
-            }
-        }
-    }
-
-    public void completeProfile() {
-        /**
-         * this method must change
-         */
-        Scanner in = new Scanner(System.in);
-        if (name == null) {
-            System.out.println("You dont have a name, want to set a name?(Y for yes and N for no)");
-            if (in.nextLine().toLowerCase().equals("y")) {
-                System.out.println("please input your full name");
-                this.setName(in.nextLine());
-            } 
-        }
-        if (email == null) {
-            System.out.println("You dont have a email, want to set a email?(Y for yes and N for no)");
-            if (in.nextLine().toLowerCase().equals("y")) {
-                System.out.println("please input your email");
-                this.setEmail(in.nextLine());
-            } 
-        }
-        if (bio == null) {
-            System.out.println("You dont have a bio, want to set a bio?(Y for yes and N for no)");
-            if (in.nextLine().toLowerCase().equals("y")) {
-                System.out.println("Please input your bio that have less than 160 character(spaces are counted)");
-                String bio = in.nextLine();
-                this.setBio(bio);
-            } 
-        }
-    }
-
-    public static void profileNotTweet(User user) {
-        /**
-         * this method must change
-         */
-        System.out.println("id : " + user.getId());
-        int flag = 0;
-        
-        if (user.getName() != null) {
-            System.out.println("Name : " + user.getName());
-            flag ++;
-        }
-        if (user.getBio() != null) {
-            System.out.println("Bio : " + user.getBio());
-            flag ++;
-        }
-
-        System.out.println("Number of followers : " + user.getFollowersNumber());
-        System.out.println("Number of followings : " + user.getFollowingsNumber());
-        System.out.println("Number of tweets : " + user.getTweetsNumber());
-        System.out.println("Number of blocked users : " + user.getBlockUsersNumber());
-
-    }
-
-    public void showFollowers() {
-        for (User user : followers) {
-            profileNotTweet(user);
-        }
-    }
-
-    public void showFollowings() {
-        for (User user : following) {
-            profileNotTweet(user);
-        }
-    }
-
-    public void showBlockUsers() {
-        for (User user : blockUsers) {
-            profileNotTweet(user);
-        }
-    }
 }
