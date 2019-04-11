5e4f2bad803c65abf77aeab502a7926c9dad957c
ali.aliabadi <al.aliabadi@mail.sbu.ac.ir>
2019-04-11 23:15:12 +0430

    add pageloader.java

---
+++ src/model/PageLoader.java
@@ -0,0 +1,27 @@
+package model;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+public class PageLoader {
+    private static final int WIDTH = 700;
+    private static final int HEIGHT = 450;
+    private static Stage stage;
+
+    public static void intiStage(Stage primarySrage) {
+        stage = primarySrage;
+        stage.setTitle("Mini Tweeter");
+        stage.setResizable(false);
+        stage.setWidth(WIDTH);
+        stage.setHeight(HEIGHT);
+    }
+
+    public void load (String url) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource(url));
+        stage.setScene(new Scene(root, WIDTH, HEIGHT));
+        stage.show();
+    }
+}
