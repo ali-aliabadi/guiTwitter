934614bede9ce35819ee9e043b6a2f491e8356e2
ali.aliabadi <al.aliabadi@mail.sbu.ac.ir>
2019-04-13 03:38:15 +0430

    add myuser field as logged user

--- src/model/Main.java
+++ src/model/Main.java
@@ -2,9 +2,12 @@ package model;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import model.User.User;
 
 
 public class Main extends Application {
+
+    public static User myUser = null;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
