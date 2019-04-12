71472d9dbaa3f543a96d41daf380f634daa079bd
ali.aliabadi <al.aliabadi@mail.sbu.ac.ir>
2019-04-13 03:42:29 +0430

    completting signup page and all its functions(only beautifulling and change colors remain

--- src/controller/SignUpController.java
+++ src/controller/SignUpController.java
@@ -1,13 +1,26 @@
 package controller;
 
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 import javafx.fxml.FXML;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import model.Main;
 import model.PageLoader;
+import model.User.User;
+import org.bson.Document;
 
 import java.io.IOException;
 
+import static com.mongodb.client.model.Filters.eq;
+
 public class SignUpController {
+
+    @FXML
+    private TextField usernamefield;
 
     @FXML
     private PasswordField password;
@@ -20,6 +33,12 @@ public class SignUpController {
 
     @FXML
     private TextField show_pass;
+
+    @FXML
+    private Text passwordmatch;
+
+    @FXML
+    private Text usernamematch;
 
     @FXML
     void hideConfirmPass() {
@@ -39,8 +58,30 @@ public class SignUpController {
     }
 
     @FXML
-    void registeruser() {
-        System.out.println(password.getText() + '\t' + confirmpassword.getText());
+    void registeruser() throws IOException {
+        String pass1 = password.getText();
+        String pass2 = confirmpassword.getText();
+        if (pass1.equals(pass2)) {
+            passwordmatch.setVisible(false);
+            if (! userIdInDB(usernamefield.getText())) {
+                signUp(usernamefield.getText(), pass1);
+                new PageLoader().load("/view/CompleteProfile.fxml");
+            } else {
+                usernamematch.setVisible(true);
+            }
+        } else {
+            passwordmatch.setVisible(true);
+            usernamematch.setVisible(false);
+        }
+    }
+
+    private boolean userIdInDB(String text) {
+
+        MongoClient mongoClient = new MongoClient();
+        MongoDatabase database = mongoClient.getDatabase("miniTweeter");
+        MongoCollection<Document> collection = database.getCollection("Users");
+
+        return collection.find(eq("username", text)).first() != null;
     }
 
     @FXML
@@ -55,6 +96,11 @@ public class SignUpController {
         show_confirmpass.setVisible(true);
         show_confirmpass.setText(password.getText());
         password.setVisible(false);
+    }
+
+    static void signUp(String id, String pass) {
+        Main.myUser = new User(id, pass);
+
     }
 
 }
