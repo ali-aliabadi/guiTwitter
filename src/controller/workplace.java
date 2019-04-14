53ebbc8dbd936775f2b0543313a400325796f7dc
ali.aliabadi <al.aliabadi@mail.sbu.ac.ir>
2019-04-15 00:20:30 +0430

    add some function to dashboard (logout, my profile, ...)

--- src/controller/workplace.java
+++ src/controller/workplace.java
@@ -1,5 +1,35 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.ImageView;
+import model.Main;
+import model.PageLoader;
+
+import java.io.IOException;
+
 public class workplace {
+
+    @FXML
+    private TextArea tweet_text;
+
+    @FXML
+    private ImageView tweet;
+
+    @FXML
+    void find_profile() {
+
+    }
+
+    @FXML
+    void log_out() throws IOException {
+        Main.myUser = null;
+        new PageLoader().load("/view/Login.fxml");
+    }
+
+    @FXML
+    void my_profile() throws IOException {
+        new PageLoader().loadProfile();
+    }
 
 }
